e535c0810b7327830b9321f323f881e92c169685
Mariano Sciacco <34033090+Maxelweb@users.noreply.github.com>
2022-01-06 23:08:59 +0100

    move cmd command in main, add test data 2

--- FakeGreenPassGenerator/main.py
+++ FakeGreenPassGenerator/main.py
@@ -1,6 +1,10 @@
 from qrgen import *
 from passgen import *
 
+import sys
+import os
+import argparse
+
 PRIVKEY = b"9d370d925476752486ab0e4a8e088228e493da12d1586fafae9f35880dbcfe03"
 HEADER = b""
 pass_data = {
@@ -33,6 +37,26 @@ pass_data = {
     }
 }
 
+pass_data2 = {-260: {1: {'dob': '1998-02-26', 
+            'nam': {'fn': 'Musterfrau-Gößinger', 
+                    'fnt': 'MUSTERFRAU<GOESSINGER',
+                    'gn': 'Gabriele', 
+                    'gnt': 'GABRIELE'},
+            't': [{'ci': 'URN:UVCI:01:AT:B5921A35D6A0D696421B3E2462178297#I', 
+                   'co': 'AT',
+                   'is': 'Ministry of Health, Austria',
+                   'nm': 'Roche LightCycler qPCR', 
+                   'ma': '1232', 
+                   'sc': '2021-02-20T12:34:56Z',
+                   'tc': 'Testing center Vienna 1',
+                   'tg': '840539006', 
+                   'tr': '260415000',
+                   'tt': 'LP6464-4'}],
+            'ver': '1.2.1'}},
+ 1: 'AT',
+ 4: 1624458597, 
+ 6: 1624285797} 
+
 
 def main():
     # QrCode
@@ -42,12 +66,34 @@ def main():
 
     for i, p in enumerate(payloads):
         pass_data[1] = p.encode()
-        msg = get_cose(pass_data)
+        msg = get_cose(pass_data2)
         msg = add_cose_key(msg, PRIVKEY)
         msg = flynn(msg.encode())
         msg = b45(msg)
         msg = b"HC1:" + msg
         print_qrs(msg, i)
+
+
+def cmd():
+    parser = argparse.ArgumentParser(description="Tool to generate Malformed QRCodes for fuzzing QRCode parsers/reader",
+                                    usage='''main.py -l [number]\nusage: main.py -w [/path/to/custom/wordlist]\n\nPayload lists:
+    0 : SQL Injections
+    1 : XSS
+    2 : Command Injection
+    3 : Format String
+    4 : XXE
+    5 : String Fuzzing
+    6 : SSI Injection
+    7 : LFI / Directory Traversal''',
+    epilog="Pay attention everywhere, even in the dumbest spot")
+    sgroup = parser.add_argument_group("Options for QRGen")
+    sgroup.add_argument("--list","-l",type=int,help="Set wordlist to use",choices=[0,1,2,3,4,5,6,7])
+    sgroup.add_argument("--wordlist","-w",type=str,default=None,help="Use a custom wordlist")
+    opt = parser.parse_args()
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    return opt
 
 if __name__ == "__main__":
     main()
--- FakeGreenPassGenerator/qrgen.py
+++ FakeGreenPassGenerator/qrgen.py
@@ -1,33 +1,10 @@
 # QrGen 
 import qrcode
 import subprocess
-import sys
-import os
-import argparse
+
 from PIL import Image
 
 lists = ['words/sqli.txt','words/xss.txt','words/cmdinj.txt','words/formatstr.txt','words/xxe.txt','words/strfuzz.txt','words/ssi.txt','words/lfi.txt']
-
-def cmd():
-    parser = argparse.ArgumentParser(description="Tool to generate Malformed QRCodes for fuzzing QRCode parsers/reader",
-                                    usage='''main.py -l [number]\nusage: qrgen.py -w [/path/to/custom/wordlist]\n\nPayload lists:
-    0 : SQL Injections
-    1 : XSS
-    2 : Command Injection
-    3 : Format String
-    4 : XXE
-    5 : String Fuzzing
-    6 : SSI Injection
-    7 : LFI / Directory Traversal''',
-    epilog="Pay attention everywhere, even in the dumbest spot")
-    sgroup = parser.add_argument_group("Options for QRGen")
-    sgroup.add_argument("--list","-l",type=int,help="Set wordlist to use",choices=[0,1,2,3,4,5,6,7])
-    sgroup.add_argument("--wordlist","-w",type=str,default=None,help="Use a custom wordlist")
-    opt = parser.parse_args()
-    if len(sys.argv) == 1:
-        parser.print_help()
-        sys.exit(1)
-    return opt
 
 def make_dirs():
     try:

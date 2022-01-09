639974b7b9e82a0cd701fd5d55c925a1560014b1
Mariano Sciacco <34033090+Maxelweb@users.noreply.github.com>
2022-01-09 15:16:34 +0100

    add boilerplate qr code fuzzer

---
+++ QRCodeFuzzer/index.js
@@ -0,0 +1,29 @@
+const wdio = require("webdriverio");
+const assert = require("assert");
+
+const opts = {
+  path: '/wd/hub',
+  port: 4723,
+  capabilities: {
+    platformName: "Android",
+    platformVersion: "30",
+    deviceName: "Android Emulator",
+    app: "./verificac19.apk",
+    appPackage: "it.ministerodellasalute.verificaC19",
+    appActivity: ".view.TextFields",
+    automationName: "UiAutomator2"
+  }
+};
+
+async function main () {
+  const client = await wdio.remote(opts);
+
+  const field = await client.$("android.widget.EditText");
+  await field.setValue("Hello World!");
+  const value = await field.getText();
+  assert.strictEqual(value,"Hello World!");
+
+  await client.deleteSession();
+}
+
+main();

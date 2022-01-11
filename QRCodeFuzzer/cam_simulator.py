02256007ef1e2fe2370a6b491315ccee1094db64
Federico Carboni <f.carboni98@gmail.com>
2022-01-11 19:04:21 +0100

    Resized cam ratio

--- QRCodeFuzzer/cam_simulator.py
+++ QRCodeFuzzer/cam_simulator.py
@@ -10,15 +10,16 @@ for filename in os.listdir(folder):
     if img is not None:
         qrcodes.append(img)
 
-HW = qrcodes[0].shape[0]
-print(HW)
-with pyvirtualcam.Camera(width=HW, height=HW, fps=30) as cam:
+h, w, _ = qrcodes[0].shape
+print(h, w)
+with pyvirtualcam.Camera(width=1920, height=1080, fps=30, fmt=pyvirtualcam.PixelFormat.RGB) as cam:
+    print('Virtual camera device: ' + cam.device)
     while True:
-        frame = qrcodes[0]
-        # frame = np.zeros((cam.height, cam.width, 3), np.uint8) # RGBA
+        # frame = qrcodes[0]
+        frame = np.zeros((cam.height, cam.width, 3), np.uint8) # RGBA
+        frame = cv2.copyMakeBorder(qrcodes[0], (cam.height-h)//2, (cam.height-h)//2, (cam.width-w)//2, (cam.width-w)//2, cv2.BORDER_CONSTANT)
         # frame[:,:,:3] = cam.frames_sent % 255 # grayscale animation
         # frame[:,:,2] = 255
         cam.send(frame)
         cam.sleep_until_next_frame()
         cam.sleep_until_next_frame()
-        

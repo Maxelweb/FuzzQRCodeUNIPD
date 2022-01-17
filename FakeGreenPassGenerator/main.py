eb15c904ae3f9ad369cd8a62a72727859cc1d928
Mariano Sciacco <34033090+Maxelweb@users.noreply.github.com>
2022-01-17 19:33:34 +0100

    Refactor 2
    - rename _old to utils
    - removed old pass_data functions

--- FakeGreenPassGenerator/main.py
+++ FakeGreenPassGenerator/main.py
@@ -5,12 +5,16 @@ from datetime import datetime
 import sys
 import argparse
 
+# TODO: test with current today date
+
 yesterday = datetime.timestamp(datetime.now()) - 86400
 tomorrow = datetime.timestamp(datetime.now()) + (7 * 86400)
 
+# --- Legend GP types ---
 # t = tested
 # v = vax
 # r = recovery
+# -----------------------
 
 fuzz_type = {
     0 : "SQL_Injections",
@@ -27,74 +31,7 @@ fuzz_type = {
 PRIVKEY = b"9d370d925476752486ab0e4a8e088228e493da12d1586fafae9f35880dbcfe03"
 HEADER = b""
 
-
-# Not sure if it is still valid
-
-pass_data = {
-    1: "DE",
-    -260: {
-        1: {
-            "v": [
-                {
-                    "dn": 2,
-                    "sd": float("inf"),
-                    "ma": "ORG-100030215",
-                    "vp": "1119349007",
-                    "dt": "2021-06-11",
-                    "co": "DE",
-                    "ci": "420_69",
-                    "mp": "EU/1/20/1528",
-                    "is": "Reichsministerium für Volksaufklärung und Propaganda",
-                    "tg": "840539006",
-                }
-            ],
-            "nam": {
-                "fnt": "Goebbels",
-                "fn": "Goebbels",
-                "gnt": "Paul Joseph",
-                "gn": "Paul Joseph",
-            },
-            "ver": "1.0.0",
-            "dob": "1897-10-29",
-        }
-    },
-}
-
-pass_data2 = {
-    -260: {
-        1: {
-            "dob": "1998-02-26",
-            "nam": {
-                "fn": "Musterfrau-Gößinger",
-                "fnt": "MUSTERFRAU<GOESSINGER",
-                "gn": "Gabriele",
-                "gnt": "GABRIELE",
-            },
-            "t": [
-                {
-                    "ci": "URN:UVCI:01:AT:B5921A35D6A0D696421B3E2462178297#I",
-                    "co": "AT",
-                    "is": "Ministry of Health, Austria",
-                    "nm": "Roche LightCycler qPCR",
-                    "ma": "1232",
-                    "sc": "2021-02-20T12:34:56Z",
-                    "tc": "Testing center Vienna 1",
-                    "tg": "840539006",
-                    "tr": "260415000",
-                    "tt": "LP6464-4",
-                }
-            ],
-            "ver": "1.2.1",
-        }
-    },
-    1: "AT",
-    4: yesterday,
-    6: tomorrow,
-}
-
-# Structure should be valid
-
-def getPass(data: str):
+def get_pass(data: str):
     return {
             4: 1683849600,
             6: 1635501173,
@@ -126,15 +63,14 @@ def getPass(data: str):
             },
         }
 
-
+# --------------------- MAIN ---------------------
 def main():
-    # QrCode
     opt = cmd()
     make_dirs()
     payloads = get_words(opt)
 
     for i, p in enumerate(payloads):
-        msg = get_cose(getPass(p))
+        msg = get_cose(get_pass(p))
         msg = add_cose_key(msg, PRIVKEY)
         msg = flynn(msg.encode(), HEADER)
         msg = b45(msg)
